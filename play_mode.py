46e492a74c9b118665138cbbd5b4e669680bacc5
icktae <taechil77@naver.com>
2023-11-12 05:46:56 +0900

    game over, game clear rule

--- play_mode.py
+++ play_mode.py
@@ -43,15 +43,15 @@ def init():
       #  game_world.add_collision_pair('zombie:ball', zombie, None)
 
     # fill here
-    global balls
-    balls = [Ball(random.randint(100, 1600 - 100), 60, 0) for _ in range(30)]
-    game_world.add_objects(balls, 1)
-
-
-
-    game_world.add_collision_pair('boy:ball', boy, None)
-    for ball in balls:
-        game_world.add_collision_pair('boy:ball', None, ball)
+    # global balls
+    # balls = [Ball(random.randint(100, 1600 - 100), 60, 0) for _ in range(30)]
+    # game_world.add_objects(balls, 1)
+    #
+    #
+    #
+    # game_world.add_collision_pair('boy:ball', boy, None)
+    # for ball in balls:
+    #     game_world.add_collision_pair('boy:ball', None, ball)
 
 
 
